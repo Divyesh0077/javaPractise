38efc123cf3cdf06287fc761b51c504cc6276eed
divyesh savaliya <divyeshsavaliya9@gmail.com>
2021-12-04 12:46:16 +0000

    branch practice

--- src/Gitpractice/Gitfirst.java
+++ src/Gitpractice/Gitfirst.java
@@ -1,2 +1,9 @@
-package Gitpractice;public class Gitfirst {
+package Gitpractice;
+
+public class Gitfirst {
+
+    // do not work on the main/dev/master
+    // Create a new branch from main
+    // Switch yor local branch
+    // and start working
 }
